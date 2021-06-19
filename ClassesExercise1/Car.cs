972f668e71a91dde9f83ab8be0662af5a2c714d1
Danielle Smith <smith.ddms@gmail.com>
2021-06-18 21:20:50 -0500

    Completed Classes Exercise 1

---
+++ ClassesExercise1/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ClassesExercise1
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+
+    }
+}
---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassesExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dot Notation
+            Car myCar = new Car();
+
+            myCar.Make = "Subaru";  
+            myCar.Model = "BRZ";
+            myCar.Year = 2018;
+
+            Console.WriteLine($"Being that {myCar.Make} is a good brand, in {myCar.Year}, I decided to purchase a new {myCar.Model}.");
+
+            //Or Object Initialization
+            var husbandCar = new Car()
+            {
+                Make = "Mercedes",
+                Model = "C350",
+                Year = 2008
+            };
+
+            Console.WriteLine($"However, my husband likes {husbandCar.Make}. He drives a {husbandCar.Model} made in {husbandCar.Year}.");
+
+            //Or Object Initializer Syntax
+            Car oldCar = new Car() { Make = "Chevy", Model = "Camero", Year = 2011 };
+
+            Console.WriteLine($"I love {oldCar.Make} as well. My last car was a {oldCar.Year} {oldCar.Model}.");
+
+            //Constructor
+            Car modelCar = new Car("Ford", "Mustang", 2021);
+
+            Console.WriteLine($"Another good brand is {modelCar.Make}. My favorite model is the {modelCar.Model}. The {modelCar.Year} model is very nice."); 
+        }
+    }
+}
